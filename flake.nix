8a83c420195c6a9eaeab48fcfbf16bb7a3d0a076
Valera <valeratrades@gmail.com>
2026-04-28 12:21:22 +0000

    docs: _

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     rust-overlay.url = "github:oxalica/rust-overlay/91e1f7a0017065360f447622d11b7ce6ed04772f";
     flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b";
     pre-commit-hooks.url = "github:cachix/git-hooks.nix/f0927703b7b1c8d97511c4116eb9b4ec6645a0fa";
-    v-utils.url = "github:valeratrades/v_flakes/8f303402daf15d26afa177d724643644c06b7d0e";
+    v-utils.url = "github:valeratrades/v_flakes?ref=v1.6";
   };
   outputs = { self, nixpkgs, rust-overlay, flake-utils, pre-commit-hooks, v-utils }:
     flake-utils.lib.eachDefaultSystem
